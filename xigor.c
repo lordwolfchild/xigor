68791ef44b0094004e30123c9b7301859546d0a0
Thomas Thiel <thiel@bit.uni-bonn.de>
2017-10-18 10:43:35 +0200

    added original pixmaps from X11 and xbiff. Removed the necessity to specify own icons and set the old pixmaps as default.

--- xigor.c
+++ xigor.c
@@ -15,6 +15,12 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+  The included XPM files are converted from the original xbm files
+  included with X11 and are subject to the X11 license and their respective
+  creators, whose names I did not find. I included these files
+  in the hope that I may do so in the spirit of FOSS.
+
 */
 
 #include <ctype.h>
@@ -25,11 +31,11 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <gtk/gtk.h>
+#include "flagup.xpm"
+#include "flagdown.xpm"
+#include "noletters.xpm"
 
 // some defaultvalues for all parameters
-#define D_SYSTRAYICON "/usr/share/icons/oxygen/base/64x64/actions/mail-receive.png"
-#define D_ACTIVEICON  "/usr/share/icons/oxygen/base/64x64/apps/internet-mail.png"
-#define D_ERRORICON   "/usr/share/icons/oxygen/base/64x64/actions/application-exit.png"
 #define D_MONITORFILE ".xigor-trigger"
 #define D_DELAY 30
 
@@ -47,13 +53,18 @@ GtkStatusIcon * st_icon;
 // storage for modifaction time of trigger file
 time_t          mod_time;
 
+// Pixbuffers for Icon storage
+GdkPixbuf *active_pixbuf;
+GdkPixbuf *error_pixbuf;
+GdkPixbuf *systray_pixbuf;
+
 // helper, returns modification time of trigger file
 // or display error icon in systray
 time_t getModTimeMonitorFile() {
   struct stat fileStat;
   if(stat(monitorfile,&fileStat) >= 0) {
     return fileStat.st_mtime;
-  } else gtk_status_icon_set_from_file(st_icon,erroricon); 
+  } else gtk_status_icon_set_from_pixbuf(st_icon,error_pixbuf); 
 };
 
 // Systray icon click callback handler
@@ -63,7 +74,7 @@ void on_icon_clicked (GtkStatusIcon *status_icon, GdkEventButton *event, gpointe
   // reset the icon to the default one. If any other button was pressed,
   // remove the systray icon and close the application.
  if (event->button==1) {
-   gtk_status_icon_set_from_file(st_icon,systrayicon);
+   gtk_status_icon_set_from_pixbuf(st_icon,systray_pixbuf);
    mod_time=getModTimeMonitorFile();
    if (debug) fprintf(stdout,"on_icon_clicked(): mod_time reset. (%d)\n",mod_time);
  } else gtk_main_quit();
@@ -76,11 +87,11 @@ static gboolean on_timeout (gpointer userdata)
 {
   if (mod_time==0) {
     mod_time=getModTimeMonitorFile();
-    gtk_status_icon_set_from_file(st_icon,systrayicon);
+    gtk_status_icon_set_from_pixbuf(st_icon,systray_pixbuf);
     if (debug) fprintf(stdout,"on_timeout(): mod_time initialized (%d)\n",mod_time);
   } else {
     if (mod_time<getModTimeMonitorFile()) {
-      gtk_status_icon_set_from_file(st_icon,activeicon);
+      gtk_status_icon_set_from_pixbuf(st_icon,active_pixbuf);
     if (debug) fprintf(stdout,"on_timeout(): mod file triggered (%d<%d)\n",mod_time,getModTimeMonitorFile());
   } else {
     if (debug) fprintf(stdout,"on_timeout(): timeout reached, no trigger (%d>=%d)\n",mod_time,getModTimeMonitorFile());
@@ -101,6 +112,7 @@ void usage() {
   fprintf(stderr,"\t-a\t\tpath to the flagged systray icon file\n");
   fprintf(stderr,"\t-e\t\tpath to the error systray icon file\n\n");
   fprintf(stderr,"spawns a flagging icon that displays the icon specified by -s in your desktop environments systray area. This icon will be changed into the one specified by -a, as soon as the file specified by -m is changed/touched. The icon specified by -e is used if the monitor file is not existing or cannot be examined by stat(). Detaches immediately after start and outputs the PID of the detached process on sdtdout before doing so.\n\n");
+  fprintf(stderr,"If you omit the icon files, the original X11 pixmaps that xbiff used are shown.\n\n");
   fprintf(stderr,"Copyright (C) 2017  Thomas Thiel\nxigor comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it within the terms of the GPLv3.\n\n");
 };
 
@@ -110,9 +122,6 @@ int main (int argc, char *argv[])
 
   // prefill config with defaults
 	delay				= D_DELAY;
-	activeicon	= D_ACTIVEICON;
-	systrayicon	= D_SYSTRAYICON;
-	erroricon		= D_ERRORICON;
  
   // get the home directory of the user
   const char *home=getenv("HOME");
@@ -192,18 +201,32 @@ int main (int argc, char *argv[])
   }
 
   // check for icon files and bail out if anyone is missing
-  if( access( activeicon, F_OK ) != 0 ) {
-    fprintf(stderr, "Activeicon file not found: %s\n", activeicon);
-    exit(150);
-  };
-  if( access( systrayicon, F_OK ) != 0 ) {
-    fprintf(stderr, "Systrayicon file not found: %s\n", systrayicon);
-    exit(150);
-  };
-  if( access( erroricon, F_OK ) != 0 ) {
-    fprintf(stderr, "Erroricon file not found: %s\n", erroricon);
-    exit(150);
-  };
+  if (activeicon!=NULL) {
+    if(access( activeicon, F_OK ) != 0 ) {
+      fprintf(stderr, "Activeicon file not found: %s\n", activeicon);
+      exit(150);
+    } else {
+      active_pixbuf = gdk_pixbuf_new_from_file(activeicon,NULL);
+    };
+  } else active_pixbuf = gdk_pixbuf_new_from_xpm_data((const char**)flagup_xpm);
+
+  if (systrayicon!=NULL) {
+    if( access( systrayicon, F_OK ) != 0 ) {
+      fprintf(stderr, "Systrayicon file not found: %s\n", systrayicon);
+      exit(150);
+    } else {
+      systray_pixbuf = gdk_pixbuf_new_from_file(systrayicon,NULL);
+    };
+  } else systray_pixbuf = gdk_pixbuf_new_from_xpm_data((const char**)flagdown_xpm);
+
+  if (erroricon!=NULL) {
+    if( access( erroricon, F_OK ) != 0 ) {
+      fprintf(stderr, "Erroricon file not found: %s\n", erroricon);
+      exit(150);
+    } else {
+      error_pixbuf = gdk_pixbuf_new_from_file(erroricon,NULL);
+    };
+  } else error_pixbuf = gdk_pixbuf_new_from_xpm_data((const char**)noletters_xpm);
 
   // init modification time of trigger file.
   mod_time=0;
@@ -212,7 +235,7 @@ int main (int argc, char *argv[])
   gtk_init (&argc, &argv);
 
   // prepare systray icon and its connections
-  st_icon = gtk_status_icon_new_from_file (systrayicon);
+  st_icon = gtk_status_icon_new_from_pixbuf (systray_pixbuf);
   g_signal_connect (st_icon, "button-press-event", G_CALLBACK (on_icon_clicked), NULL);
 
   // prepare calling of our delay-expiration-handler
